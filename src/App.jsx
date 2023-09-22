400d3793e7ea3d9a804efed399e5c96e2efcd5a6
tiendp <hoangdangtien1108@gmail.com>
2023-09-22 13:48:24 +0700

    upload code

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="result">0</div>
+        <div className="contents">
+          
+          <div className="content" style={{backgroundColor:'gray'}}>C</div>
+          <div className="content" style={{backgroundColor:'gray'}}>+</div>
+          <div className="content" style={{backgroundColor:'gray'}}>%</div>
+          <div className="content" style={{backgroundColor:'orange'}}>/</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>7</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>8</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>9</div>
+          <div className="content" style={{backgroundColor:'orange'}}>*</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>4</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>5</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>6</div>
+          <div className="content" style={{backgroundColor:'orange'}}>-</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>1</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>2</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>3</div>
+          <div className="content" style={{backgroundColor:'orange'}}>+</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)', flex:'50%', flex:'50%'}}>0</div>
+          <div className="content" style={{backgroundColor:'rgb(224, 224, 224)'}}>.</div>
+          <div className="content" style={{backgroundColor:'orange'}}>=</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
